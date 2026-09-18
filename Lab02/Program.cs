896616e6282b9bd2f16b71e2b761a3ad8af2ac1b
polinadiv <gavryshdaughter@gmail.com>
2026-09-18 15:07:38 +0300

    Lab02 Task04

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -2,4 +2,5 @@
 
 //Task1.Run();
 //Task2.Run();
-Task3.Run();
+//Task3.Run();
+Task4.Run();
---
+++ Lab02/Task4.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Lab02;
+
+internal class Task4
+{
+    public static void Run()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[n, m];
+
+        int maxValue = int.MinValue;
+        int maxRow = 0;
+        int maxCol = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] parts = Console.ReadLine().Split(' ');
+
+            for (int j = 0; j < m; j++)
+            {
+                matrix[i, j] = int.Parse(parts[j]);
+
+                if (matrix[i, j] > maxValue)
+                {
+                    maxValue = matrix[i, j];
+                    maxRow = i;
+                    maxCol = j;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            int rowSum = 0;
+
+            for (int j = 0; j < m; j++)
+            {
+                rowSum += matrix[i, j];
+            }
+
+            Console.WriteLine($"Лікар {i + 1}: {rowSum} прийомів");
+        }
+
+        Console.Write("По днях: ");
+
+        for (int j = 0; j < m; j++)
+        {
+            int colSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                colSum += matrix[i, j];
+            }
+
+            Console.Write(colSum);
+
+            if (j < m - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine(
+            $"Максимум: {maxValue} (Лікар {maxRow + 1}, День {maxCol + 1})");
+    }
+}
