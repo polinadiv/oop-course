809da098fa471535c35f50d8362ff2d0a6506022
polinadiv <gavryshdaughter@gmail.com>
2026-09-18 15:23:34 +0300

    Lab02 Task05

---
+++ Lab02/Task5.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lab02;
+
+internal class Task5
+{
+    public static void Run()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[n, n];
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] parts = Console.ReadLine().Split(' ');
+
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i, j] = int.Parse(parts[j]);
+            }
+        }
+
+        int[] mainDiagonal = new int[n];
+        int[] secondaryDiagonal = new int[n];
+
+        int mainSum = 0;
+        int secondarySum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            mainDiagonal[i] = matrix[i, i];
+            secondaryDiagonal[i] = matrix[i, n - 1 - i];
+
+            mainSum += mainDiagonal[i];
+            secondarySum += secondaryDiagonal[i];
+        }
+
+        Console.WriteLine(
+            $"Головна діагональ: {string.Join(", ", mainDiagonal)} (сума = {mainSum})");
+
+        Console.WriteLine(
+            $"Побічна діагональ: {string.Join(", ", secondaryDiagonal)} (сума = {secondarySum})");
+    }
+}
