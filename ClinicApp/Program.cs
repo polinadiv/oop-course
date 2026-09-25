0cc00b50480bbcf6dee610efeeb7625af483867c
polinadiv <gavryshdaughter@gmail.com>
2026-09-25 19:34:01 +0300

    Lab03 Task02

--- ClinicApp/Program.cs
+++ ClinicApp/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace ClinicApp;
+﻿/* namespace ClinicApp;
 
 public class Program
 {
@@ -36,5 +36,74 @@ public class Program
         Console.WriteLine(p3);
         Console.WriteLine(p4);
         Console.WriteLine(p5);
+    }
+} */
+namespace ClinicApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Doctor doctor1 = new Doctor(
+                "Олег",
+                "Сидоренко",
+                "Кардіологія",
+                "LIC-001",
+                "+380501111111");
+
+            Doctor doctor2 = new Doctor(
+                "Наталія",
+                "Мороз",
+                "Неврологія",
+                "LIC-002",
+                "+380502222222");
+
+            Doctor doctor3 = new Doctor(
+                "Андрій",
+                "Власенко",
+                "Педіатрія",
+                "LIC-003",
+                "+380503333333");
+
+            Doctor doctor4 = new Doctor(
+                "Ірина",
+                "Коваль",
+                "Дерматологія",
+                "LIC-004",
+                "+380504444444");
+
+            doctor2.WorkStartHour = 10;
+            doctor2.WorkEndHour = 19;
+
+            doctor3.WorkStartHour = 7;
+            doctor3.WorkEndHour = 15;
+
+            Console.WriteLine(doctor1);
+            Console.WriteLine(doctor2);
+            Console.WriteLine(doctor3);
+            Console.WriteLine(doctor4);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Доступні зараз:");
+
+            Doctor[] doctors =
+            {
+                doctor1,
+                doctor2,
+                doctor3,
+                doctor4
+            };
+
+            for (int i = 0; i < doctors.Length; i++)
+            {
+                if (doctors[i].IsAvailableNow)
+                {
+                    Console.WriteLine(doctors[i].FullName);
+                }
+            }
+
+            Console.WriteLine();
+        }
     }
 }
